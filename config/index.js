4fc840a70d0bb9c054ea95dad15a3ca2782f3711
Faisal Al-Tameemi <faisal.al.tameemi@gmail.com>
2016-11-19 02:26:49 -0500

    added lookup util functions

--- config/index.js
+++ config/index.js
@@ -2,5 +2,6 @@
 
 module.exports = {
 	mongo_uri: 'mongodb://localhost:27017/harvester',
-	port: 3000
+	port: 3000,
+	full_contact_api_key: '66f07bf1bdc204e5'
 };
---
+++ util/lookup.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const config = require('../config');
+const axios = require('axios');
+
+// create an axios instance to make HTTP calls to full contact APIs
+const fullContactClient = axios.create({
+  baseURL: 'https://api.fullcontact.com/v2/',
+	headers: {
+		'X-FullContact-APIKey': config.full_contact_api_key
+	}
+});
+
+/**
+	lookup a person by their full name
+*/
+const nameLookup = (name, _done) => {
+	fullContactClient.get('/name/normalizer.json', {
+	  params: {
+			q: name
+		}
+	})
+	.then(resp => _done(null, resp))
+	.catch(_done);
+}
+
+/**
+	lookup a person by their email
+*/
+const emailLookup = (email, _done) => {
+	fullContactClient.get('/person.json', {
+	  params: {
+			email
+		}
+	})
+	.then(resp => _done(null, resp))
+	.catch(_done);
+}
+
+/**
+	lookup a company by its domain
+*/
+const domainLookup = (domain, _done) => {
+	fullContactClient.get('/company/lookup.json', {
+	  params: {
+			domain
+		}
+	})
+	.then(resp => {
+		_done(null, resp)
+	})
+	.catch(_done);
+}
+
+nameLookup('Faisal Al-Tameemi', (err, resp) => {
+	debugger;
+});
+
+module.exports = {
+	nameLookup,
+	emailLookup,
+	domainLookup
+};
